805266f141679dda8a0ad682f8d8ebd39054c814
chang-seop <wpqnr1221@naver.com>
2022-07-01 19:42:24 +0900

    Rest API 추가 1. 조회는 GET 요청을 통해서 article의 목록 조회 2. 단일 article을 조회 /api/articles/{id} 3. 생성 post /api/articles 4. 수정 patch /api/articles/{id} 5. 삭제 delete 진행 /api/articles/{id} Controller : 요청을 받았을 때 뷰 템플릿 페이지(HTML) 반환 RestController : 요청을 받았을 때 JSON 파일이 반환, 또는 Text 반환(데이터 반환)

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,10 +2,14 @@ package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@Setter
 public class ArticleForm { //Form 데이터를 받아 올 그릇
 
     private Long id; //id 필드 추가
